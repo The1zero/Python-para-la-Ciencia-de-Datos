f9f1f6ea0bfafc09023f7f49a74d41f5497e1c47
The1zero <chandos562@gmail.com>
2026-06-23 15:44:38 -0600

    Dia 11

---
+++ Matplotlib/Histograma.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Graficar una distribucion normal
+data=np.random.normal(170,10,200) #muestra de datos array distribucion normal(media de la distribucion en los parametros, luego la desviacion estandar y le pidemos a numpy que cree 200 datos)
+
+#Graficar informacion (histograma)
+plt.hist(data, color='Salmon', edgecolor='black', alpha=0.6)
+plt.title('Distribucion de altura')
+plt.xlabel('Altura (cm)')
+plt.ylabel('Densidad')   
+plt.show()
